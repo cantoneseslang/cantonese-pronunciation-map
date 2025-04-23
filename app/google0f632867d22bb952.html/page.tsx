124914f9e4e29741fb410d0a6a59ddd025f4e972
bestinksalesman <bestinksalesman@gmail.com>
2025-04-23 17:26:01 +0800

    fix: update Google verification page

---
+++ app/google0f632867d22bb952.html/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-static'
+
+export default function GoogleVerification() {
+  return (
+    <div dangerouslySetInnerHTML={{ __html: 'google-site-verification: google0f632867d22bb952.html' }} />
+  )
+} 
--- app/google0f632867d22bb952.html/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export async function GET() {
-  return new NextResponse('google-site-verification: google0f632867d22bb952.html', {
-    headers: {
-      'Content-Type': 'text/html',
-    },
-  })
-} 
