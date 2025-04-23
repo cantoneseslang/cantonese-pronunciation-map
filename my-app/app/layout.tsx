60a3dec1cfe69c3a4950b4dae747f3bb60b8e244
bestinksalesman <bestinksalesman@gmail.com>
2025-04-23 15:49:05 +0800

    feat: add Vercel Analytics

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import StyledComponentsRegistry from "@/lib/registry"
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="ja">
       <body className={inter.className}>
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        <Analytics />
       </body>
     </html>
   )
